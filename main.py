066f4a6e13e1f00d06a34c58e0713d0a0fb4b2d7
monkeyx10x <panchenko.yarikx10x@gmail.com>
2026-05-29 20:55:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
 import pandas as pd
 from io import StringIO
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Marketplace SaaS",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
